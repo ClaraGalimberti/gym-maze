e046b44dc5cca8939c00be08d1a0608b9c1f0911
Clara <clara.galimberti@epfl.ch>
2022-10-31 14:15:00 +0100

    pygame version indication is not needed

--- setup.py
+++ setup.py
@@ -10,7 +10,13 @@ setup(name="gym_maze",
           "gym_maze.envs": ["maze_samples/*.npy"]
       },
       install_requires=['gym==0.15.7',
-                        'pygame==1.9.6',
+                        'pygame',
                         'numpy']
       )
 
+# If pygame is not installing, run: brew install sdl sdl_image sdl_mixer sdl_ttf portmidi
+# or
+# export CC='/usr/bin/gcc'
+# export CFLAGS='-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk -I/opt/X11/include -arch i386 -arch x86_64'
+# export LDFLAGS='-arch i386 -arch x86_64'
+# export ARCHFLAGS='-arch i386 -arch x86_64'
