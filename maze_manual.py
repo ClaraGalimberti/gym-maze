694b555b7ccc36077049ffd34df8eaa4457219cc
Clara <clara.galimberti@epfl.ch>
2022-11-09 18:46:10 +0100

    change to 7x7 default

--- maze_manual.py
+++ maze_manual.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
 
     # Initialize the "maze" environment
     # env = gym.make("maze-sample-5x5-v0")
-    env = gym.make("maze-sample-5x5-v1")
+    # env = gym.make("maze-sample-5x5-v1")
     # env = gym.make("maze-sample-7x7-v0")
     # env = gym.make("maze-random-5x5-v0")
-    # env = gym.make("maze-random-7x7-v0")
+    env = gym.make("maze-random-7x7-v0")
     # env = gym.make("maze-random-10x10-loop-v0")
 
     MAZE_SIZE = tuple((env.observation_space.high + np.ones(env.observation_space.shape)).astype(int))
